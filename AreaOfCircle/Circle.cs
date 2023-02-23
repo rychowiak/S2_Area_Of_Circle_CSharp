78af1932ce244baae4db1dd643ab077a664ff1c1
richard <ricardosepu93@gmail.com>
2023-02-22 18:32:35 -0700

    Add project files.

---
+++ AreaOfCircle/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfCircle
+{
+    internal class Circle
+    {
+        public static void CircleArea(double r, double pi)
+        {
+            double a = pi * r * r;
+            Console.WriteLine("The area of a circle of radius {0} is: {1:0.00}", r, a);
+        }
+        public static double CircleCircumference(double r, double pi)
+        {
+            double c = 2 * pi * r;
+            Console.WriteLine("The circumference of a circle of radius {0} is: {1:0.00}", r, c);
+            return c;
+        }
+
+        public static void CircleDiameter(double r)
+        {
+            double d = 2 * r;
+            Console.WriteLine("The diameter of a circle of radius {0} is: {1}", r, d);
+        }
+    }
+}
---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,45 @@
+﻿using AreaOfCircle;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double radius;
+        while(true)
+        {
+            Console.Write("Enter radius of circle: ");
+            radius = Convert.ToDouble(Console.ReadLine());
+            if (radius < 1)
+            {
+                Console.WriteLine("Please enter a positive number.");
+                continue;
+            }
+            else
+                break;
+        }
+
+
+        Circle.CircleArea(radius, 3.14);
+        double circumference = Circle.CircleCircumference(radius, 3.14);
+        Circle.CircleDiameter(radius);
+
+        double km, litersConsumed;
+
+        while (true)
+        {
+            Console.Write("How many km per liters your car consumes? (only km): ");
+            km = Convert.ToDouble(Console.ReadLine());
+
+            if (km < 1)
+            {
+                Console.WriteLine("Please enter positive number");
+                continue;
+            }
+            else
+                break;
+        }
+
+        litersConsumed = circumference / km;
+        Console.WriteLine("Assuming the radius entered is in km, your car will use {0:0.00} liters of gas to go around the circle.", litersConsumed);
+    }
+}
